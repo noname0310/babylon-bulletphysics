6d85577dba3381bbd3eb62f5496195340c9da018
noname0310 <hjnam2014@gmail.com>
2024-10-13 14:53:36 +0900

    create motion state binding

--- src/wasm_src/src/bind/motion_state.rs
+++ src/wasm_src/src/bind/motion_state.rs
@@ -1,3 +1,5 @@
+use glam::Vec3A;
+
 #[link(name = "bullet")]
 extern "C" {
     fn bw_create_motion_state(transform_buffer: *const f32) -> *mut std::ffi::c_void;
@@ -11,6 +13,15 @@ extern "C" {
     fn bw_motion_state_bundle_get_motion_states_ptr(bundle: *const std::ffi::c_void) -> *const *mut std::ffi::c_void;
 }
 
+#[repr(C, align(16))]
+struct MotionStateRaw {
+    vtable: *const std::ffi::c_void,
+    matrix_rowx: Vec3A,
+    matrix_rowy: Vec3A,
+    matrix_rowz: Vec3A,
+    translation: Vec3A,
+}
+
 pub(crate) struct MotionState {
     ptr: *mut std::ffi::c_void,
 }
@@ -22,6 +33,16 @@ impl MotionState {
             ptr: unsafe { bw_create_motion_state(transform_buffer.as_ptr()) },
         }
     }
+
+    pub(crate) fn get_world_transform(&self) -> glam::Mat4 {
+        let raw = unsafe { &*(self.ptr as *const MotionStateRaw) };
+        glam::Mat4::from_cols(
+            glam::Vec4::new(raw.matrix_rowx.x, raw.matrix_rowy.x, raw.matrix_rowz.x, 0.0),
+            glam::Vec4::new(raw.matrix_rowx.y, raw.matrix_rowy.y, raw.matrix_rowz.y, 0.0),
+            glam::Vec4::new(raw.matrix_rowx.z, raw.matrix_rowy.z, raw.matrix_rowz.z, 0.0),
+            glam::Vec4::new(raw.translation.x, raw.translation.y, raw.translation.z, 1.0),
+        )
+    }
 }
 
 impl Drop for MotionState {
@@ -32,13 +53,50 @@ impl Drop for MotionState {
 
 pub(crate) struct MotionStateBundle {
     ptr: *mut std::ffi::c_void,
+    size: usize,
 }
 
 impl MotionStateBundle {
     pub(crate) fn new(count: usize) -> Self {
         Self {
             ptr: unsafe { bw_create_motion_state_bundle(count) },
+            size: count,
         }
+    }
+
+    pub(crate) fn init_motion_state(&self, index: usize, transform: &glam::Mat4) {
+        let motion_states_ptr = unsafe {
+            bw_motion_state_bundle_get_motion_states_ptr(self.ptr) as *mut MotionStateRaw
+        };
+
+        let motion_state_ptr = unsafe { motion_states_ptr.add(index) };
+        
+        let transform_buffer = transform.as_ref();
+        let raw = unsafe { &mut *motion_state_ptr };
+        raw.matrix_rowx = Vec3A::new(transform_buffer[0], transform_buffer[4], transform_buffer[8]);
+        raw.matrix_rowy = Vec3A::new(transform_buffer[1], transform_buffer[5], transform_buffer[9]);
+        raw.matrix_rowz = Vec3A::new(transform_buffer[2], transform_buffer[6], transform_buffer[10]);
+        raw.translation = Vec3A::new(transform_buffer[12], transform_buffer[13], transform_buffer[14]);
+    }
+
+    pub(crate) fn get_world_transform(&self, index: usize) -> glam::Mat4 {
+        let motion_states_ptr = unsafe {
+            bw_motion_state_bundle_get_motion_states_ptr(self.ptr) as *mut MotionStateRaw
+        };
+
+        let motion_state_ptr = unsafe { motion_states_ptr.add(index) };
+        let raw = unsafe { &*motion_state_ptr };
+        
+        glam::Mat4::from_cols(
+            glam::Vec4::new(raw.matrix_rowx.x, raw.matrix_rowy.x, raw.matrix_rowz.x, 0.0),
+            glam::Vec4::new(raw.matrix_rowx.y, raw.matrix_rowy.y, raw.matrix_rowz.y, 0.0),
+            glam::Vec4::new(raw.matrix_rowx.z, raw.matrix_rowy.z, raw.matrix_rowz.z, 0.0),
+            glam::Vec4::new(raw.translation.x, raw.translation.y, raw.translation.z, 1.0),
+        )
+    }
+
+    pub(crate) fn size(&self) -> usize {
+        self.size
     }
 }
 
--- src/wasm_src/src/lib.rs
+++ src/wasm_src/src/lib.rs
@@ -23,6 +23,24 @@ pub fn init() {
     BULLET_GLOBAL_CTORS.call_once(|| {
         unsafe { __wasm_call_ctors() };
     });
+
+    let mat4 = glam::Mat4::from_cols(
+        glam::Vec4::new(1.0, 2.0, 3.0, 4.0),
+        glam::Vec4::new(5.0, 6.0, 7.0, 8.0),
+        glam::Vec4::new(9.0, 10.0, 11.0, 12.0),
+        glam::Vec4::new(13.0, 14.0, 15.0, 16.0),
+    );
+    let motion_state = bind::motion_state::MotionState::new(&mat4);
+    let world_transform = motion_state.get_world_transform();
+    web_sys::console::log_1(&format!("world_transform: {:?}", world_transform).into());
+
+    let mation_state_bundle = bind::motion_state::MotionStateBundle::new(2);
+    mation_state_bundle.init_motion_state(0, &mat4);
+    mation_state_bundle.init_motion_state(1, &mat4);
+    let world_transform = mation_state_bundle.get_world_transform(0);
+    web_sys::console::log_1(&format!("world_transform: {:?}", world_transform).into());
+    let world_transform = mation_state_bundle.get_world_transform(1);
+    web_sys::console::log_1(&format!("world_transform: {:?}", world_transform).into());
 }
 
 #[wasm_bindgen(js_name = "createBulletRuntime")]
