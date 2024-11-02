6a7718ff9a74140590a8c7573df80853bcd8db19
noname0310 <hjnam2014@gmail.com>
2024-11-02 23:58:59 +0900

    physics world c++ to rust ffi

--- src/wasm_src/src/bind/mod.rs
+++ src/wasm_src/src/bind/mod.rs
@@ -1,3 +1,4 @@
 pub(super) mod collision_shape;
 pub(super) mod motion_state;
+pub(super) mod physics_world;
 pub(super) mod rigidbody;
---
+++ src/wasm_src/src/bind/physics_world.rs
@@ -0,0 +1,48 @@
+#[link(name = "bullet")]
+extern "C" {
+    fn bw_create_world() -> *mut std::ffi::c_void;
+
+    fn bw_destroy_world(world: *mut std::ffi::c_void);
+
+    fn bw_world_set_gravity(world: *mut std::ffi::c_void, x: f32, y: f32, z: f32);
+
+    fn bw_world_step_simulation(world: *mut std::ffi::c_void, time_step: f32, max_sub_steps: i32, fixed_time_step: f32);
+
+    fn bw_world_add_rigidbody(world: *mut std::ffi::c_void, rigidbody: *mut std::ffi::c_void);
+
+    fn bw_world_remove_rigidbody(world: *mut std::ffi::c_void, rigidbody: *mut std::ffi::c_void);
+}
+
+pub(crate) struct PhysicsWorld {
+    ptr: *mut std::ffi::c_void,
+}
+
+impl PhysicsWorld {
+    pub(crate) fn new() -> Self {
+        Self {
+            ptr: unsafe { bw_create_world() },
+        }
+    }
+
+    pub(crate) fn set_gravity(&mut self, x: f32, y: f32, z: f32) {
+        unsafe { bw_world_set_gravity(self.ptr, x, y, z) };
+    }
+
+    pub(crate) fn step_simulation(&mut self, time_step: f32, max_sub_steps: i32, fixed_time_step: f32) {
+        unsafe { bw_world_step_simulation(self.ptr, time_step, max_sub_steps, fixed_time_step) };
+    }
+
+    pub(crate) fn add_rigidbody(&mut self, rigidbody: *mut std::ffi::c_void) {
+        unsafe { bw_world_add_rigidbody(self.ptr, rigidbody) };
+    }
+
+    pub(crate) fn remove_rigidbody(&mut self, rigidbody: *mut std::ffi::c_void) {
+        unsafe { bw_world_remove_rigidbody(self.ptr, rigidbody) };
+    }
+}
+
+impl Drop for PhysicsWorld {
+    fn drop(&mut self) {
+        unsafe { bw_destroy_world(self.ptr) };
+    }
+}
