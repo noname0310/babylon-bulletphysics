d30f70508b187d3af12bc114bd996216a05d783a
noname0310 <hjnam2014@gmail.com>
2024-11-03 15:33:00 +0900

    update test scene

--- src/Test/sceneBuilder.ts
+++ src/Test/sceneBuilder.ts
@@ -8,12 +8,15 @@ import { DirectionalLight } from "@babylonjs/core/Lights/directionalLight";
 import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
 import { ShadowGenerator } from "@babylonjs/core/Lights/Shadows/shadowGenerator";
 import { Color3, Color4 } from "@babylonjs/core/Maths/math.color";
-import { Matrix, Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { Matrix, Quaternion, Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { CreateBox } from "@babylonjs/core/Meshes/Builders/boxBuilder";
+import { CreatePlane } from "@babylonjs/core/Meshes/Builders/planeBuilder";
 import { Scene } from "@babylonjs/core/scene";
+import { Inspector } from "@babylonjs/inspector";
 
 import { getBulletWasmInstance } from "@/Runtime/bulletWasmInstance";
 import { BulletWasmInstanceTypeMD } from "@/Runtime/InstanceType/multiDebug";
-import { PhysicsBoxShape } from "@/Runtime/physicsShape";
+import { PhysicsBoxShape, PhysicsStaticPlaneShape } from "@/Runtime/physicsShape";
 import { PhysicsWorld } from "@/Runtime/physicsWorld";
 import { RigidBody } from "@/Runtime/rigidBody";
 import { RigidBodyBundle } from "@/Runtime/rigidBodyBundle";
@@ -28,10 +31,10 @@ export class SceneBuilder implements ISceneBuilder {
         const scene = new Scene(engine);
         scene.clearColor = new Color4(0.95, 0.95, 0.95, 1.0);
 
-        const camera = new ArcRotateCamera("arcRotateCamera", 0, 0, 500, new Vector3(0, -50, 0), scene);
+        const camera = new ArcRotateCamera("arcRotateCamera", 0, 0, 500, new Vector3(0, 0, 0), scene);
         camera.minZ = 1;
-        camera.maxZ = 5000;
-        camera.setPosition(new Vector3(300, 500, -500));
+        camera.maxZ = 500;
+        camera.setPosition(new Vector3(30, 50, -50));
         camera.attachControl(undefined, false);
         camera.inertia = 0.8;
         camera.speed = 10;
@@ -43,8 +46,8 @@ export class SceneBuilder implements ISceneBuilder {
 
         const directionalLight = new DirectionalLight("directionalLight", new Vector3(0.5, -1, 1), scene);
         directionalLight.intensity = 0.5;
-        directionalLight.shadowMaxZ = 300;
-        directionalLight.shadowMinZ = -300;
+        directionalLight.shadowMaxZ = 30;
+        directionalLight.shadowMinZ = -30;
 
         const shadowGenerator = new ShadowGenerator(2048, directionalLight, true);
         shadowGenerator.transparencyShadow = true;
@@ -53,38 +56,50 @@ export class SceneBuilder implements ISceneBuilder {
         shadowGenerator.bias = 0.004;
         shadowGenerator.filteringQuality = ShadowGenerator.QUALITY_MEDIUM;
 
+        Inspector.Show(scene, { enablePopup: false });
+
         const wasmInstance = await getBulletWasmInstance(new BulletWasmInstanceTypeMD());
-
-        const boxShape = new PhysicsBoxShape(wasmInstance, new Vector3(50, 50, 50));
-        boxShape;
-
-        const rbInfo = new RigidBodyConstructionInfo(wasmInstance);
-        rbInfo.shape = boxShape;
-        rbInfo.setInitialTransform(new Matrix().set(
-            1, 2, 3, 4,
-            5, 6, 7, 8,
-            9, 10, 11, 12,
-            13, 14, 15, 16
-        ));
-
-        const rigidBody = new RigidBody(wasmInstance, rbInfo);
-        rigidBody;
-
-        const rbInfoList = new RigidBodyConstructionInfoList(wasmInstance, 2);
-        rbInfoList.setShape(0, boxShape);
-        rbInfoList.setShape(1, boxShape);
-
-        const rigidBodyBundle = new RigidBodyBundle(wasmInstance, rbInfoList);
-        rigidBodyBundle;
-
         const world = new PhysicsWorld(wasmInstance);
 
-        world.addRigidBody(rigidBody);
-        world.addRigidBodyBundle(rigidBodyBundle);
+        const matrix = new Matrix();
 
-        for (let i = 0; i < 100; ++i) {
-            world.stepSimulation(1 / 60, 10, 1 / 60);
+        {
+            const ground = CreatePlane("ground", { size: 600 }, scene);
+            ground.rotationQuaternion = Quaternion.RotationAxis(new Vector3(1, 0, 0), Math.PI / 2);
+            shadowGenerator.addShadowCaster(ground);
+            ground.receiveShadows = true;
+
+            const groundShape = new PhysicsStaticPlaneShape(wasmInstance, new Vector3(0, 1, 0), 0);
+            const groundRbInfo = new RigidBodyConstructionInfo(wasmInstance);
+            groundRbInfo.shape = groundShape;
+            groundRbInfo.setInitialTransform(Matrix.FromQuaternionToRef(ground.rotationQuaternion, matrix));
+
+            const groundRigidBody = new RigidBody(wasmInstance, groundRbInfo);
+            world.addRigidBody(groundRigidBody);
         }
+
+        {
+            const baseBox = CreateBox("box", { size: 2 }, scene);
+            // baseBox.isEnabled(false);
+            baseBox.scaling.set(1, 1, 1);
+            shadowGenerator.addShadowCaster(baseBox);
+            baseBox.receiveShadows = true;
+            const boxShape = new PhysicsBoxShape(wasmInstance, new Vector3(50, 50, 0));
+
+            const rbCount = 1024;
+            const rbInfoList = new RigidBodyConstructionInfoList(wasmInstance, rbCount);
+            for (let i = 0; i < rbCount; ++i) {
+                rbInfoList.setShape(i, boxShape);
+                rbInfoList.setInitialTransform(i, Matrix.Translation(0, i * 2, 0));
+            }
+
+            const bundle = new RigidBodyBundle(wasmInstance, rbInfoList);
+            world.addRigidBodyBundle(bundle);
+        }
+
+        scene.onBeforeRenderObservable.add(() => {
+            // world.stepSimulation(1 / 60, 10, 1 / 60);
+        });
 
         return scene;
     }
