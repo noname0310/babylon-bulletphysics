d7915ffe607edaa5c093fea654b04bd7e3261918
noname0310 <hjnam2014@gmail.com>
2024-12-15 16:43:48 +0900

    add ammo.js for benchmark

--- postInstallScript.ts
+++ postInstallScript.ts
@@ -1,6 +1,48 @@
 import { execSync } from "child_process";
 import * as fs from "fs";
 
+async function download(url: string, dest: string): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        const file = fs.createWriteStream(dest);
+        https.get(url, (response) => {
+            response.pipe(file);
+            file.on("finish", () => {
+                file.close();
+                resolve();
+            });
+        }).on("error", (err) => {
+            fs.unlink(dest, () => reject(err));
+        });
+    });
+}
+
+async function installAmmo(): Promise<void> {
+    if (fs.existsSync("./src/External/ammo.wasm.d.ts")) return;
+
+    await download(
+        "https://github.com/regnaio/ammo.js/tree/37cff85841b7ee79180114c9da7a32ed8d2ae549/dist/ammo.wasm.js",
+        "./src/External/ammo.wasm.js"
+    );
+    await download(
+        "https://github.com/regnaio/ammo.js/tree/37cff85841b7ee79180114c9da7a32ed8d2ae549/dist/ammo.wasm.wasm",
+        "./src/External/ammo.wasm.wasm"
+    );
+    await download(
+        "https://raw.githubusercontent.com/giniedp/ammojs-typed/05408a318256ca561720aad1cfd0e83e772f06cb/ammo/ammo.d.ts",
+        "./src/External/ammo.wasm.d.ts"
+    );
+
+    try {
+        execSync("cd src/External && git init && git apply ammo-bundler.patch");
+    } catch (error: any) {
+        console.log(error.output.toString());
+        process.exit(1);
+    }
+    if (fs.existsSync("./src/External/.git")) {
+        fs.rmSync("./src/External/.git", { recursive: true });
+    }
+}
+
 async function installBullet(): Promise<void> {
     if (!fs.existsSync("./src/wasm_src/bullet_src/BulletCollision")) {
         try {
@@ -40,6 +82,7 @@ async function installBullet(): Promise<void> {
 }
 
 async function main(): Promise<void> {
+    await installAmmo();
     await installBullet();
 }
 
