3df1a9712203e8003dfea95080ada0aa1f838d52
noname0310 <hjnam2014@gmail.com>
2025-04-26 23:53:37 +0900

    export symbols

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-// theres no export default here yet
+
+// Runtime/Impl
+export { IPhysicsRuntime } from "@/Runtime/Impl/IPhysicsRuntime";
+export { MultiPhysicsRuntime } from "@/Runtime/Impl/multiPhysicsRuntime";
+export { NullPhysicsRuntime } from "@/Runtime/Impl/nullPhysicsRuntime";
+export { PhysicsRuntime } from "@/Runtime/Impl/physicsRuntime";
+export { PhysicsRuntimeEvaluationType } from "@/Runtime/Impl/physicsRuntimeEvaluationType";
+
+// Runtime/InstanceType
+export { BulletWasmInstanceTypeMD } from "@/Runtime/InstanceType/multiDebug";
+export { BulletWasmInstanceTypeMR } from "@/Runtime/InstanceType/multiRelease";
+export { BulletWasmInstanceTypeSD } from "@/Runtime/InstanceType/singleDebug";
+export { BulletWasmInstanceTypeSR } from "@/Runtime/InstanceType/singleRelease";
+
+// Runtime/Plugin
+export { BulletPlugin } from "@/Runtime/Plugin/bulletPlugin";
+
+// Runtime
+export { BulletWasmInstance } from "@/Runtime/bulletWasmInstance";
+export { Constraint, Generic6DofConstraint, Generic6DofSpringConstraint } from "@/Runtime/constraint";
+export { MotionType } from "@/Runtime/motionType";
+export { MultiPhysicsWorld } from "@/Runtime/multiPhysicsWorld";
+export { PhysicsBoxShape, PhysicsCapsuleShape, PhysicsShape, PhysicsSphereShape, PhysicsStaticPlaneShape } from "@/Runtime/physicsShape";
+export { PhysicsWorld } from "@/Runtime/physicsWorld";
+export { RigidBody } from "@/Runtime/rigidBody";
+export { RigidBodyBundle } from "@/Runtime/rigidBodyBundle";
+export { RigidBodyConstructionInfo } from "@/Runtime/rigidBodyConstructionInfo";
+export { RigidBodyConstructionInfoList } from "@/Runtime/rigidBodyConstructionInfoList";
